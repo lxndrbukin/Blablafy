95e511bbf4d4357c6aed03617481f3f8bb5108fe
Alexander Bukin <alex.bukin@hotmail.com>
2022-08-03 08:08:12 +0100

    Adding MongoDB. Need to install Mongoose later due to slow internet connection

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./services/keys');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(keys.mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('CONNECTED TO MONGODB'))
+  .catch((error) => console.log(error));
 
 const PORT = 5000;
 app.listen(PORT, console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
