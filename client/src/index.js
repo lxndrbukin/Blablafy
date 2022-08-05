3660d326849d4e33c2268b2992e6826b6d1d0396
Alexander <lxndrbukin@gmail.com>
2022-08-05 16:55:37 +0100

    Connecting Redux to the Client Side

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './components/App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,10 @@
+export const userReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'CREATE_USER':
+      return state, action.payload;
+    case 'FETCH_USER':
+      return state, action.payload;
+    default:
+      return state;
+  }
+};
