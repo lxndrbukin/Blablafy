63c5935afaea112b48bbaa368889675cc1a3d76f
Alexander Bukin <alex.bukin@hotmail.com>
2022-08-08 08:04:17 +0100

    Making minor adjustments to the UI

--- client/src/components/TopBar/TopBarUser/TopBarUser.jsx
+++ client/src/components/TopBar/TopBarUser/TopBarUser.jsx
@@ -26,21 +26,28 @@ const TopBarUser = ({ currentUser, logoutUser, fetchUser }) => {
     );
   };
 
+  const userDetails = () => {
+    if (currentUser) {
+      <div className='top-bar_user-profile_details'>
+        <div className='top-bar_user-profile_name'>{currentUser.username}</div>
+        <div className='top-bar_user-profile_status'>Status</div>
+      </div>;
+    }
+  };
+
   const miniProfile = () => {
     return (
-      <div className='top-bar_user-profile'>
-        <div
-          className='top-bar_user-profile_avatar'
-          style={{
-            backgroundImage:
-              "url('https://www.savoric.com/wp-content/uploads/2018/03/profil-pic_dummy.png')",
-          }}
-        ></div>
+      <div className='top-bar_user-profile-wrapper'>
+        <div className='top-bar_user-profile'>
+          <div
+            className='top-bar_user-profile_avatar'
+            style={{
+              backgroundImage:
+                "url('https://www.savoric.com/wp-content/uploads/2018/03/profil-pic_dummy.png')",
+            }}
+          ></div>
+        </div>
         {auth()}
-        {/* <div className='top-bar_user-profile_details'>
-          <div className='top-bar_user-profile_name'>Name</div>
-          <div className='top-bar_user-profile_status'>Status</div>
-        </div> */}
       </div>
     );
   };
