21a4571c1fa51c4fc861e79f58df0af7cc7b34c3
Alexander <lxndrbukin@gmail.com>
2022-08-12 17:50:38 +0100

    Working on UI. Adding changes that were not saved.

--- client/src/components/Profile/ProfileDetails/ProfileDetailsRight.jsx
+++ client/src/components/Profile/ProfileDetails/ProfileDetailsRight.jsx
@@ -8,7 +8,13 @@ const ProfileDetailsRight = ({ username }) => {
           <div className='profile-info_name'>{username}</div>
           <div className='profile-info_status'>Set your status</div>
         </div>
-        <div className='profile-info_box-line'></div>
+        <div className='profile-info_box-line'>
+          <div className='profile-info_details'>
+            <div className='profile-info_details-line'>
+              <span></span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
